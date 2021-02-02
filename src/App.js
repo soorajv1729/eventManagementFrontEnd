2814c9f556c94dd421db96bb8c83071539967877
sooraj <sooraj@revyrieglobal.com>
2021-02-02 06:43:17 -0800

    routing applied

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
 import { ButtonCustom } from './components/button';
+import {   BrowserRouter as Router,
+   Link, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <ButtonCustom/>
-      </header>
+    <Router>
+    <div>
+      <nav>
+        <ul>
+         
+          <li>
+            <Link to="/button">Button</Link>
+          </li>
+         
+        </ul>
+      </nav>
+
+      {/* A <Switch> looks through its children <Route>s and
+          renders the first one that matches the current URL. */}
+      <Switch>
+        <Route path="/button">
+          <ButtonCustom />
+        </Route>
+       
+      </Switch>
     </div>
+  </Router>
   );
 }
 
